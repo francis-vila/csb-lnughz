cc3f15954173e5089b466bb90e96dc2b4f7e7198
Code Sandbox <hello@codesandbox.io>
2022-12-01 15:46:17 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.66e5d6105361660f4e674347209850fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b1277e901060f881735cef82f29ea2",
+    "url": "https://francis-vila.github.io/csb-lnughz/index.html"
+  },
+  {
+    "revision": "7b2d9696c527d5abfb2e",
+    "url": "https://francis-vila.github.io/csb-lnughz/static/js/2.ed6ce5cf.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://francis-vila.github.io/csb-lnughz/static/js/2.ed6ce5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a67406803b757c087d",
+    "url": "https://francis-vila.github.io/csb-lnughz/static/js/main.bbedb53e.chunk.js"
+  },
+  {
+    "revision": "7e95594039ff57c8cf81",
+    "url": "https://francis-vila.github.io/csb-lnughz/static/js/runtime-main.c6e0429a.js"
+  }
+]);
